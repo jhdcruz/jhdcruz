885fd074798e41eb0b823478502460973f01863e
jhdcruz <jhdcrux@outlook.com>
2022-01-26 20:57:08 +0800

    build: generate sitemaps on build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Enable the Solid renderer to support Solid JSX components.
   renderers: ["@astrojs/renderer-solid"],
+  buildOptions: {
+    sitemap: true,
+    site: "https://jhdcruz.pages.dev",
+  },
 });
