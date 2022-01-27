826bc22d90035e7b2c35152372dbd481877907b7
jhdcruz <jhdcrux@outlook.com>
2022-01-27 23:01:25 +0800

    chore: add tw custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   darkMode: "class",
   content: ["./public/**/*.html", "./src/**/*.{astro,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        matte: "#1e1e1e",
+        dark: "#0f0f0f",
+        primary: "#fac641",
+      },
+    },
   },
   plugins: [],
 };
