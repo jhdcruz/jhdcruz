51f31497620852c2666c890676af16b17ec4a1ca
jhdcruz <jhdcrux@outlook.com>
2022-01-27 23:48:56 +0800

    chore: fix linters and formatters

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  darkMode: "class",
-  content: ["./public/**/*.html", "./src/**/*.{astro,tsx}"],
+  darkMode: 'class',
+  content: ['./public/**/*.html', './src/**/*.{astro,tsx}'],
   theme: {
     extend: {
       colors: {
-        matte: "#1e1e1e",
-        dark: "#0f0f0f",
-        primary: "#fac641",
+        matte: '#1e1e1e',
+        dark: '#0f0f0f',
+        primary: '#fac641',
       },
     },
   },
