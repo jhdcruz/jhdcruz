5c6060a7efe963d003d98e22f0b168c599a07d0a
jhdcruz <jhdcrux@outlook.com>
2022-02-09 15:49:12 +0800

    feat(web): experimental app navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,120 +1,57 @@
-import { createSignal, Index, Show } from 'solid-js';
+import { Index } from 'solid-js';
 
 interface IRoutes {
   name: string;
   url: string;
+  icon: string;
 }
 
 const links: IRoutes[] = [
   {
     name: 'About',
     url: '#about',
+    icon: 'user',
   },
   {
     name: 'Projects',
     url: '#projects',
+    icon: 'cube-transparent',
+  },
+  {
+    name: 'Contact',
+    url: 'mailto:jhdcrux@outlook.com',
+    icon: 'mail',
   },
   {
     name: 'Skills',
     url: '#skills',
+    icon: 'puzzle',
   },
   {
     name: 'Badges',
     url: '#badges',
+    icon: 'flag',
   },
 ];
 
-const MobileNav = () => {
-  const [menu, setMenu] = createSignal(false);
-
-  const openMenu = () => setMenu(!menu());
-
-  return (
-    <>
-      <button aria-label="Open navigation button" onClick={openMenu}>
-        <Show
-          when={menu()}
-          fallback={
-            <svg
-              xmlns="<http://www.w3.org/2000/svg>"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              display="block"
-            >
-              <path d="M3 6h18M3 12h18M3 18h18" />
-            </svg>
-          }
-        >
-          <svg
-            xmlns="<http://www.w3.org/2000/svg>"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            display="block"
-          >
-            <path d="M20 20L4 4m16 0L4 20" />
-          </svg>
-        </Show>
-      </button>
-
-      <Show when={menu()}>
-        <ul class="w-full" arial-label="Navigation links">
-          <Index each={links}>
-            {(link: () => IRoutes) => (
-              <li class="rounded-md py-2 px-3">
-                <a
-                  class="decoration-primary decoration-2 underline-offset-8 hover:underline focus:outline-primary"
-                  href={link().url}
-                  aria-label={`Link to ${link().name}`}
-                >
-                  {link().name}
-                </a>
-              </li>
-            )}
-          </Index>
-        </ul>
-      </Show>
-    </>
-  );
-};
-
-const DesktopNav = () => (
-  <div arial-label="Navigation links">
-    <Index each={links}>
-      {(link: () => IRoutes) => (
-        <a
-          class=" rounded-md py-2 px-3 decoration-primary decoration-2 underline-offset-8 hover:underline focus:outline-primary"
-          href={link().url}
-          aria-label={`Link to ${link().name}`}
-        >
-          {link().name}
-        </a>
-      )}
-    </Index>
-  </div>
-);
-
-// TODO: Merge navigation
 export const Nav = () => (
-  <nav class=" border-b border-gray-300 bg-white p-5 dark:bg-matte sticky top-0 w-screen">
-    <div class="space-between flex">
-      <span class="sm:hidden height-fit">
-        <MobileNav />
-      </span>
-      <span class="hidden sm:block">
-        <DesktopNav />
-      </span>
+  <nav class="flex justify-center p-3 border-t md:border-b md:border-t-0 border-gray-300 bg-white dark:bg-matte absolute bottom-0 md:top-0 md:bottom-auto w-screen">
+    <div class="flex w-full" arial-label="Navigation links">
+      <Index each={links}>
+        {(link: () => IRoutes) => (
+          <a
+            class="rounded-lg p-3 text-gray-600 text-center grow"
+            href={link().url}
+            aria-label={`Link to ${link().name}`}
+          >
+            <span
+              class="iconify mx-auto text-xl mb-1"
+              data-icon={`heroicons-solid:${link().icon}`}
+            />
+            {link().name}
+          </a>
+        )}
+      </Index>
     </div>
   </nav>
 );
