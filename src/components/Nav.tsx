d51b39a7f5a4e99a49d9a17b3bdb325386623201
jhdcruz <jhdcrux@outlook.com>
2022-04-07 22:18:50 +0800

    fix: partial fix to icon content reflow onload

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -38,7 +38,7 @@ export const Nav = () => (
   <nav class="bg-transparent fixed bottom-0 md:top-0 md:bottom-auto w-screen">
     {/* Mobile nav */}
     <div
-      class="flex mb-3 mx-4 border shadow-xl rounded-lg bg-white dark:bg-matte md:hidden"
+      class="flex mb-3 mx-4 border shadow-xl rounded-lg bg-white dark:bg-matte min-h-[66px] md:hidden"
       arial-label="Navigation links"
     >
       <Index each={links}>
@@ -56,45 +56,6 @@ export const Nav = () => (
           </a>
         )}
       </Index>
-    </div>
-
-    {/* Desktop nav */}
-    <div class="max-w-6xl mx-auto px-10 hidden md:block">
-      <div class="flex justify-between">
-        <div class="flex">
-          {/* Website Logo */}
-          <a href="/" class="flex items-center py-4 px-2">
-            <img src="logo.png" alt="Logo" class="h-8 w-8 mr-2" />
-            <span class="font-semibold text-lg">{/* Website Title */}</span>
-          </a>
-        </div>
-
-        {/* Primary Navbar items */}
-        <div class="hidden md:flex items-center x-space-1">
-          <Index each={links}>
-            {(link: () => IRoutes) => (
-              <a
-                href={link().url}
-                class="py-4 px-2 mx-3 rounded-lg text-gray-600 text-center"
-              >
-                {link().name}
-              </a>
-            )}
-          </Index>
-        </div>
-
-        {/* CTA items */}
-        <div class="hidden md:flex items-center">
-          <div class="items-center">
-            <a
-              href="signup"
-              class="py-2 px-4 mx-2 font-medium text-white bg-green-500 rounded-lg"
-            >
-              Sign Up
-            </a>
-          </div>
-        </div>
-      </div>
     </div>
   </nav>
 );
