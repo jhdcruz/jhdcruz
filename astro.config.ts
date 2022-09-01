6140965a19361604635af936e66d1d96ea0b7a9a
jhdcruz <jhdcrux@outlook.com>
2022-09-01 15:21:04 +0800

    deps: migrate to astro v1

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,32 @@
 import { defineConfig } from 'astro/config';
-import solid from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   integrations: [
-    solid(),
-    tailwind({ config: { path: './tailwind.config.cjs' } }),
+    tailwind({
+      config: {
+        path: './tailwind.config.cjs',
+      },
+    }),
+    sitemap({
+      customPages: ['https://jhdcruz.pages.dev'],
+    }),
+    partytown({}),
   ],
   site: 'https://jhdcruz.pages.dev',
-  server: { port: 3000, host: true },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  vite: {
+    ssr: {
+      external: ['svgo'],
+    },
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   darkMode: 'class',
   content: ['./public/**/*.html', './src/**/*.{astro,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      display: ['Lato', ...defaultTheme.fontFamily.sans],
+      body: ['Lato', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         matte: '#1e1e1e',
