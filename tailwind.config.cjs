5259484eb65044277c5337ed6d7766584094d25d
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-02-05 13:03:31 +0800

    style: change font to `Inter`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,17 +5,17 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{astro,tsx}'],
   theme: {
     fontFamily: {
-      sans: ['Cantarell', ...defaultTheme.fontFamily.sans],
-      display: ['Cantarell', ...defaultTheme.fontFamily.sans],
-      body: ['Cantarell', ...defaultTheme.fontFamily.sans],
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      display: ['Inter', ...defaultTheme.fontFamily.sans],
+      body: ['Inter', ...defaultTheme.fontFamily.sans],
     },
     extend: {
       colors: {
-        matte: '#1e1e1e',
-        dark: '#0f0f0f',
-        light: '#f8f9fa',
-        primary: '#f1ab48',
-        'primary-darken': '#ca8f3c',
+        matte: '#2e1e1e',
+        dark: '#1f0f0f',
+        light: '#f9f9fa',
+        primary: '#f2ab48',
+        'primary-darken': '#ca9f3c',
       },
     },
   },
