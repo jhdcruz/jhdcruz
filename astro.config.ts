46b9b7b6d40daa1483a9f1eaba66c9d7050e37d4
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-02-14 10:31:11 +0800

    fix: function-oriented output

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare({ mode: 'directory' }),
   integrations: [
     tailwind({
       config: {
