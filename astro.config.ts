53d93190f72a3309d8d3a62c15439fb34b90de53
Joshua Hero Dela Cruz <jhdcrux@outlook.com>
2023-02-14 10:02:18 +0800

    config: set cloudflare adapter to directory mode

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({ mode: 'directory' }),
   integrations: [
     tailwind({
       config: {
