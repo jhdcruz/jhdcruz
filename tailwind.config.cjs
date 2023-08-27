6f62f75719c8ffb22ce71fe6d9e3a656f5f8b629
Joshua Hero <jhdcruz@proton.me>
2023-08-27 13:34:50 +0800

    fix: inter font not found

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter Variable', 'Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         matte: '#2e1e1e',
