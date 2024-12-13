29db6c8201e84c5fbca0ae8483f9c7b1fccf7710
jhdcruz <jhdcruz@proton.me>
2024-12-13 19:31:02 +0800

    move kv bindings inside astro components

--- functions/status.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Return availability status from Cloudflare KV store.
- */
-export const onRequest: PagesFunction<Env> = async (context) => {
-	const status = await context.env.jhdcruz.get("status");
-	return new Response(status);
-};
---
+++ src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="astro/client" />
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+type ENV = {
+	jhdcruz: KVNamespace;
+};
+
+// use a default runtime configuration (advanced mode).
+type Runtime = import("@astrojs/cloudflare").Runtime<ENV>;
+declare namespace App {
+	interface Locals extends Runtime {}
+}
