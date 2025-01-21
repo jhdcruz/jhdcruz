ffe3bc955933ec75c076271cca2c1ec5897247ce
jhdcruz <jhdcruz@proton.me>
2025-01-21 12:04:30 +0800

    chore(config): replace biome with stylistic eslint

---
+++ eslint.config.ts
@@ -0,0 +1,26 @@
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
+import astro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
+export default tseslint.config(
+	tseslint.configs.strict,
+	{
+		files: ["**/*.d.ts"],
+		rules: {
+			"@typescript-eslint/no-empty-object-type": "off",
+		},
+	},
+	jsxA11y.flatConfigs.recommended,
+	[
+		{
+			plugins: {
+				"@stylistic": stylistic,
+			},
+			rules: {
+				"@stylistic/indent": ["warn", "tab"],
+			},
+		},
+		...astro.configs.recommended,
+	],
+);
