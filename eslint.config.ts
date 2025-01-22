736bae98b80379085d5b7918d0c45c3cdc30d3b5
jhdcruz <jhdcruz@proton.me>
2025-01-22 23:42:19 +0800

    refactor: go back to spaces

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,23 +4,21 @@ import astro from "eslint-plugin-astro";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 
 export default tseslint.config(
-	tseslint.configs.strict,
-	{
-		files: ["**/*.d.ts"],
-		rules: {
-			"@typescript-eslint/no-empty-object-type": "off",
-		},
-	},
-	jsxA11y.flatConfigs.recommended,
-	[
-		{
-			plugins: {
-				"@stylistic": stylistic,
-			},
-			rules: {
-				"@stylistic/indent": ["warn", "tab"],
-			},
-		},
-		...astro.configs.recommended,
-	],
+    tseslint.configs.strict,
+    {
+        files: ["**/*.d.ts"],
+        rules: {
+            "@typescript-eslint/no-empty-object-type": "off",
+        },
+    },
+    jsxA11y.flatConfigs.recommended,
+    ...astro.configs.recommended,
+    {
+        plugins: {
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "@stylistic/indent": ["warn", 4],
+        },
+    },
 );
